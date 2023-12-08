0c92ff531c240f487db9567a2d4602f76e69ed0a
Muhammed Muneef M <md.muneefnk@gmail.com>
2023-12-08 12:49:43 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/ev_companion/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ev_companion
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/constants/color_constants/color_constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class ColorConstants {
+  static Color primaryColor = Color.fromARGB(255, 46, 139, 87);
+  static Color primaryWhite = Colors.white;
+  static Color primaryBlack = Colors.black;
+}
